55e6bf39fb36823afca585c0a47108738c98f6e6
keerthiram2612 <keerthiram2612@gmail.com>
2023-08-27 20:23:10 +0530

    creating the header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <div className="logo">
+        </div>
+    <div className="section1">
+        <ul className="home-section">
+        <li>Home</li>
+        <li>Studentdetails</li>
+        <li>Contact</li>
+        <li>Login</li>
+        </ul>
+        </div>
+    </div>
+  )
+}
