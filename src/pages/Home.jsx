2237efd448d18a364d2b0b3fbd9cc7dfa40566e9
keerthiram2612 <keerthiram2612@gmail.com>
2023-08-28 09:47:50 +0530

    updating the file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,9 @@ function Home() {
     <div className='home-section'>
         <div className='background-image'></div>
         <h1 className='home-image'>WELCOME TO JAYA ENGINEERING COLLEGE</h1>
+        <p className='home-para'>Approved by AICTE,New Delhi | Affiliated to Anna University, chennai<br/>
+         A Telugu Minority Institute<br/>
+        </p>
         <div className="home-content">
             <h2 className='content'></h2>
             <p></p>
