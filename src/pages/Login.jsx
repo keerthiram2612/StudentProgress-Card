b753a87ae7af785efccc30879070c0d2973304c9
keerthiram2612 <keerthiram2612@gmail.com>
2023-09-13 06:39:43 +0530

    updated the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <div className='login'>
       <div className="login-page ">
-        <h1>Login</h1>
+        <h1 className='login-h1'>Login</h1>
         <i class="fa-solid fa-user fa-2xl"></i><input className="user"type="text" placeholder='Enter your username' /><br/>
         <i class="fa-solid fa-lock fa-2xl"></i><input className="pass" type="text" placeholder='Enter password' /><br/>
          <button className='btn'>Login</button><p>Or</p><br/>
