5b7a62c72e06ea432eec32e4d89b043580943b73
keerthiram2612 <keerthiram2612@gmail.com>
2023-09-14 20:42:35 +0530

    modifying the contact page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -3,8 +3,6 @@ import React from 'react'
 function ContactUs() {
   return (
     <div>
-        <h1 className='contact-div'>Jaya Engineering College</h1>
-        <img className='contact-img' src="https://pbs.twimg.com/profile_images/1471912001970446337/b8fXDap9_400x400.jpg" alt="" />
     <div className='container-contactus'>
         <h1 className='contactus-h1'>Contact Us</h1>
         <h2  className='contactus-h2'>College Address</h2>
@@ -14,22 +12,22 @@ function ContactUs() {
       </p>
       <p> Whatsapp : +91-85310 77777<br/>
       Email : admission@jec.ac.in, info@jec.ac.in<br/>
-      Director      : 9788833336,
-      Vice Principal  : 9551329915
+      Director      : 9788833336,<br/>
+      Vice Principal  : 9551329915<br/>
       Placement Director : +91 9361194459<br/>
-      Corporate Office
-      # 108, Pondy Bazar,
+      Corporate Office<br/>
+      # 108, Pondy Bazar,<br/>
       Rajalakshmi Mahal, III rd floor,
-      T.Nagar, Chennai 600 017.
+      T.Nagar, Chennai 600 017.<br/>
       Tamil Nadu, India.
       Telephone : +91-44-2815 5531
        Fax       : +91-44-5212 3995</p>
        <h2 className='contactus-h2'>Jaya Group of instutions</h2>
        <p>CTH Road, Thiruninravur,
-       Chennai – 602 024.Tamilnadu, India.
+       Chennai – 602 024.Tamilnadu, India.<br/>
        Phone +91 44 26390982
       +91 44 26390682
-      +91 44 26300707
+      +91 44 26300707<br/>
       Email ID vc@jec.ac.in
       Website www.jayagc.org</p>
        </div>
